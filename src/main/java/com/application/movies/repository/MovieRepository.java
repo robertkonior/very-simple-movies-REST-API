4ebdcbdfa632acec22005a2569f0bb4b0a4dfbed
Robert Konior <praca.robert.konior@gmail.com>
2018-04-25 00:07:21 +0200

    checking ddl.auto=create

--- src/main/java/com/application/movies/repository/MovieRepository.java
+++ src/main/java/com/application/movies/repository/MovieRepository.java
@@ -2,12 +2,11 @@ package com.application.movies.repository;
 
 import com.application.movies.domain.Movie;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
     @Override
